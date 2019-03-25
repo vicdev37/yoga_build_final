82f355b43ffe3f7915bad5e5dc48a7202ff6c440
Victor <justicejesus1237@gmail.com>
2019-03-26 02:54:59 +0300

    yoga fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+require('es6-promise').polyfill();
+require('formdata-polyfill');
 import calc from './parts/calc';
 import form from './parts/form';
 import slider from './parts/slider';
@@ -16,3 +18,13 @@ window.addEventListener('DOMContentLoaded', function () {
 	timer();
 	modal();
 });	
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+	console.info('polyfill for IE11');
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
